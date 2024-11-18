a2912b95ef4a769f33944cc7636d3f6694dea84d
20211144 <gkskdml7419@gmail.com>
2024-11-18 18:46:30 +0900

    song output

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ function App() {
   return (
     <div>
       <Header/>
+      <Playlist/>
+      <Container/>
     </div>
   )
 }
@@ -11,6 +13,29 @@ function App() {
 function Header() {
   return (
     <h1>React 프로그래밍</h1>
+  )
+}
+
+function Playlist() {
+  return (
+    <div className="playlist">
+      프로그래밍하면서 듣고 싶은 노래
+    </div>
+  )
+}
+
+function Container() {
+  return (
+    <div className='container'>
+        <a href="https://www.youtube.com/results?search_query=링딩동">
+          <img 
+            src='https://picsum.photos/600/150?random=1'
+            alt='랜덤 이미지 1'/>
+          <div className='song-title'>
+            링딩동
+          </div>
+        </a>
+    </div>
   )
 }
 
