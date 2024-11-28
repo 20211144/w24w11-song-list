a49f537eb9df643a953bec3cd7d87f9303e9e8ad
20211144 <gkskdml7419@gmail.com>
2024-11-28 14:11:07 +0900

    ✨ 가사 보이기 및 숨기기 처리

--- src/Container.js
+++ src/Container.js
@@ -1,20 +1,29 @@
+import { useState } from "react"
+
 const Container = (props) => {
+  const [lyricsExpanded, setLyricsExpanded] = useState(false)
+  const togglelyrics = () => {
+    setLyricsExpanded(prevState => !prevState)
+  }
     return (
       <>
         <div className='container'>
+        <img 
+                src={`https://picsum.photos/600/150?random=${props.song.id}`}
+                alt={`${props.song.title} 이미지`}
+                onClick={togglelyrics}/>
             <a href={`https://www.youtube.com/results?search_query=${props.song.title}`} 
             target="_black"
             rel="noreferrer">
-              <img 
-                src={`https://picsum.photos/600/150?random=${props.song.id}`}
-                alt={`${props.song.title} 이미지`}/>
               <div className='song-title'>
                 {props.song.title} ({props.song.singer})
               </div>
             </a>
         </div>
-        {props.song.lyrics && (
-          <pre className="song-lyrics">
+        {props.song.lyrics && lyricsExpanded &&(
+          <pre 
+          className="song-lyrics"
+          onClick={togglelyrics}>
             {props.song.lyrics}  
           </pre>
         )}
