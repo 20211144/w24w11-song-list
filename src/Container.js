468a9e76a1168ea329c4a514f5fabab014e03b55
20211144 <gkskdml7419@gmail.com>
2024-11-28 14:30:45 +0900

    🍱 선호도 출력

--- src/Container.js
+++ src/Container.js
@@ -1,4 +1,5 @@
 import { useState } from "react"
+import {FaStar} from "react-icons/fa"
 
 const Container = (props) => {
   const [lyricsExpanded, setLyricsExpanded] = useState(false)
@@ -19,6 +20,13 @@ const Container = (props) => {
                 {props.song.title} ({props.song.singer})
               </div>
             </a>
+            <div className="song-rating">
+              {[...Array(props.song.rating)].map((_,index) =>(
+                <FaStar key={index}/>
+              )
+            )
+              }    
+            </div>
         </div>
         {props.song.lyrics && lyricsExpanded &&(
           <pre 
